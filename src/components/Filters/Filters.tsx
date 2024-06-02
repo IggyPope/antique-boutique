36c297301855ead4225984ef6c68d1ac29378e65
Anastasia Koval <144329946+kovalkl@users.noreply.github.com>
2024-06-02 17:34:25 +0300

    feat: implement filters view (#105)

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,14 +1,38 @@
 import { useState } from 'react';
 
-import { FormControl, InputLabel, MenuItem, Select, Stack, Typography } from '@mui/material';
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  Slider,
+  Stack,
+  Typography,
+} from '@mui/material';
 
 import { Category } from '@commercetools/platform-sdk';
 
+import MultiplySelect from '@/components/Filters/MultiplySelect';
 import categories from '@/hooks/useCategories';
+
+const brandsData = ['Reebok', 'Adidas', 'Nike', 'Lacoste', 'Braun'];
+const sizeData = ['S', 'M', 'L', 'One size'];
+const colorData = ['Red', 'Blue', 'Green', 'Black', 'White', 'Colorful'];
+const MIN_MAX_PRICE = [0, 100];
 
 const Filters = () => {
   const { getMainCategories, getSubcategories } = categories();
+  const [currentCategory, setCurrentCategory] = useState('all');
+  const [currentSubcategory, setCurrentSubcategory] = useState('');
   const [subcategories, setSubcategories] = useState<Category[]>([]);
+  const [price, setPrice] = useState<number[]>(MIN_MAX_PRICE);
+  const [brands, setBrands] = useState<string[]>([]);
+  const [sizes, setSizes] = useState<string[]>([]);
+  const [colors, setColors] = useState<string[]>([]);
+  const [inStock, setInStock] = useState(false);
 
   const mainCategories = getMainCategories();
 
@@ -20,12 +44,23 @@ const Filters = () => {
     setSubcategories(subcategories);
   };
 
+  const resetFilters = () => {
+    setCurrentCategory('all');
+    setCurrentSubcategory('');
+    setSubcategories([]);
+    setPrice(MIN_MAX_PRICE);
+    setBrands([]);
+    setSizes([]);
+    setColors([]);
+    setInStock(false);
+  };
+
   return (
     <Stack gap={2}>
       <Typography>Shopping Options</Typography>
       <FormControl variant="standard">
         <InputLabel>Category</InputLabel>
-        <Select>
+        <Select value={currentCategory}>
           <MenuItem key="All" value="all" onClick={() => setSubcategories([])}>
             All
           </MenuItem>
@@ -34,7 +69,10 @@ const Filters = () => {
               <MenuItem
                 key={category.id}
                 value={category.name['en-US']}
-                onClick={() => handleCategoryChange(category.id)}
+                onClick={() => {
+                  setCurrentCategory(category.name['en-US']);
+                  handleCategoryChange(category.id);
+                }}
               >
                 {category.name['en-US']}
               </MenuItem>
@@ -43,14 +81,44 @@ const Filters = () => {
       </FormControl>
       <FormControl variant="standard">
         <InputLabel>Subcategory</InputLabel>
-        <Select disabled={!subcategories.length}>
+        <Select disabled={!subcategories.length} value={currentSubcategory}>
           {subcategories.map((category) => (
-            <MenuItem key={category.id} value={category.name['en-US']}>
+            <MenuItem
+              key={category.id}
+              value={category.name['en-US']}
+              onClick={() => setCurrentSubcategory(category.name['en-US'])}
+            >
               {category.name['en-US']}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
+      <MultiplySelect label="Brand" state={brands} itemsData={brandsData} setState={setBrands} />
+      <MultiplySelect label="Size" state={sizes} itemsData={sizeData} setState={setSizes} />
+      <MultiplySelect label="Color" state={colors} itemsData={colorData} setState={setColors} />
+      <Stack m={1}>
+        <Typography ml={-1}>Price Range</Typography>
+        <Slider
+          value={price}
+          onChange={(_, newValue) => {
+            if (typeof newValue === 'number') {
+              return;
+            }
+            setPrice(newValue);
+          }}
+          valueLabelDisplay="auto"
+        />
+      </Stack>
+      <FormControlLabel
+        control={<Checkbox checked={inStock} onChange={() => setInStock(!inStock)} />}
+        label="In Stock"
+      />
+      <Stack direction="row" gap={2}>
+        <Button variant="contained">Apply Filters</Button>
+        <Button variant="outlined" onClick={resetFilters}>
+          Reset
+        </Button>
+      </Stack>
     </Stack>
   );
 };
