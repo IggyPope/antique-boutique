7bf05bf8aa597310a9ea9fbb7388b2b2d9611305
Igor Vakulchik <altko4egar@gmail.com>
2024-06-18 14:37:56 +0300

    feat: implement clear cart confirmation dialog (#149)

--- src/components/Cart/CartList.tsx
+++ src/components/Cart/CartList.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
+
 import { Button, Stack } from '@mui/material';
 
 import { LineItem } from '@commercetools/platform-sdk';
 
 import CartItem from '@/components/Cart/CartItem';
+import { ConfirmationModal } from '@/components/Cart/ConfirmationModal';
 import { useCart } from '@/hooks/useCart';
 
 const CartList = ({ isLoading, cartItems }: { isLoading: boolean; cartItems: LineItem[] }) => {
   const { updateItemQuantity, clearCart } = useCart();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleClearCart = async () => {
     await clearCart();
@@ -17,20 +22,36 @@ const CartList = ({ isLoading, cartItems }: { isLoading: boolean; cartItems: Lin
   };
 
   return (
-    <Stack gap={2}>
-      {cartItems.map((item) => (
-        <CartItem key={item.id} item={item} changeQuantity={changeQuantity} isLoading={isLoading} />
-      ))}
-      <Button
-        onClick={handleClearCart}
-        disabled={isLoading}
-        color="error"
-        variant="outlined"
-        sx={{ marginLeft: 'auto' }}
-      >
-        Clear Shopping Cart
-      </Button>
-    </Stack>
+    <>
+      <Stack gap={2}>
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            changeQuantity={changeQuantity}
+            isLoading={isLoading}
+          />
+        ))}
+        <Button
+          onClick={() => setIsModalOpen(true)}
+          disabled={isLoading}
+          color="error"
+          variant="outlined"
+          sx={{ marginLeft: 'auto' }}
+        >
+          Clear Shopping Cart
+        </Button>
+      </Stack>
+
+      <ConfirmationModal
+        isOpen={isModalOpen}
+        handleCancel={() => setIsModalOpen(false)}
+        handleConfirm={() => {
+          setIsModalOpen(false);
+          void handleClearCart();
+        }}
+      />
+    </>
   );
 };
 
